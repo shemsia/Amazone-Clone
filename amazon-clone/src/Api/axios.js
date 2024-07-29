e306a8a8bc372d3bdef1257b6837896ce262820c
shemsia <shemsia63@gmail.com>
2024-07-29 07:09:41 +0300

    url

--- amazon-clone/src/Api/axios.js
+++ amazon-clone/src/Api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-f086f/us-central1/api",
-  //baseURL:"https://amazon-api-deploy-1-eeul.onrender.com"
+  // baseURL: "http://127.0.0.1:5001/clone-f086f/us-central1/api",
+  baseURL:"https://amazon-api-deploy-1-eeul.onrender.com"
 });
 
 export { axiosInstance };
